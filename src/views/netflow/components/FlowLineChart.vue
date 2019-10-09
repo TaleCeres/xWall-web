2478002394e905c2eb584932be2ce0b2f1c1ade3
LuMengli <mengfaluo@163.com>
2019-10-09 19:33:50 +0800

    feat: 网络状态页面内容

---
+++ src/views/netflow/components/FlowLineChart.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :id="id" style="height: 400px"></div>
+</template>
+
+<script>
+import resize from '@/mixins/resize'
+
+export default {
+  name: 'FlowLineChart',
+  mixins: [resize],
+  props: {
+    id: {
+      type: String,
+      default: 'FlowLineChart'
+    },
+    valueList: {
+      type: Array,
+      default: () => []
+    },
+    dateList: {
+      type: Array,
+      default: () => []
+    },
+  },
+  data() {
+    return {
+      chart: null,
+    }
+  },
+  computed: {
+    option() {
+      return {
+        xAxis: {
+          type: 'category',
+          data: this.dateList
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '流量',
+            data: this.valueList,
+            type: 'line',
+            smooth: true
+          }]
+      }
+    }
+  },
+  watch: {
+    option(prev, next) {
+      this.updateChart()
+    }
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
