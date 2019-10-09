11bc48df6470ccbca0b85235e97f5c5e73f2f38e
qiufeihong <qiufeihong2018@126.com>
2019-10-09 11:28:47 +0800

    feat(finish user style):

--- src/views/user/user-list.vue
+++ src/views/user/user-list.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>用户管理</div>
+  <div class='user-container'>
+    <el-table :data='dataList' border :header-cell-style="{'background-color':'#F2F2F2'}">
+      <el-table-column prop='username' label='用户名' />
+      <el-table-column prop='authority' label='权限' />
+      <el-table-column label='操作'>
+        <template>
+          <div>
+            <i class='el-icon-delete'></i>
+          </div>
+        </template></el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'UserList'
-}
+  export default {
+    name: 'UserList',
+    data() {
+      return {
+        dataList: [{
+          username: 'admin',
+          authority: '管理员'
+        }, {
+          username: 'auditor',
+          authority: '用户'
+        }]
+      }
+    },
+  }
 </script>
 
 <style scoped lang="stylus">
-
+.user-container{
+  margin:30px
+}
+  .el-icon-delete {
+    background-color: #6F060b;
+    color: #ffffff;
+    padding:2px
+  }
 </style>
--- src/views/user/user-reset.vue
+++ src/views/user/user-reset.vue
@@ -1,13 +1,127 @@
 <template>
-  <div>修改密码</div>
+  <div class='user-container'>
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="200px"
+      class="user-ruleForm">
+      <el-form-item label="用户名" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="新密码" prop="pass">
+        <el-input v-model="ruleForm.pass" type='password' autocomplete='off'
+          placeholder='密码最小长度8位 字母+数字'></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input v-model="ruleForm.checkPass" type='password' autocomplete='off'></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="btn-edit" @click="submitForm('ruleForm')">修改密码</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'UserReset'
-}
+  export default {
+    name: 'UserReset',
+    data() {
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        ruleForm: {
+          name: '',
+          pass: '',
+          checkPass: ''
+        },
+        rules: {
+          name: [{
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          }],
+          pass: [{
+            required: true,
+            validator: validatePass,
+            trigger: 'blur'
+          }],
+          checkPass: [{
+            required: true,
+            validator: validatePass2,
+            trigger: 'blur'
+          }]
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      }
+    }
+  }
 </script>
 
-<style scoped lang="stylus">
+<style lang="stylus">
+  .user-container {
+    margin: 30px
+  }
 
+  .el-form-item__label {
+    text-align: center;
+    border-radius: 4px;
+    background-color: #FBFBFB;
+    border: 1px solid #DCDFE6;
+  }
+
+  .el-input__inner {
+    margin-left: 10px;
+  }
+
+  .el-select {
+    width: 100%;
+  }
+
+  .btn-edit {
+    background-color: #289E90;
+    color: #ffffff;
+    font-size: 15px;
+    margin-left: -201px;
+  }
+
+  .btn-edit:hover,
+  .btn-edit:active,
+  .btn-edit:focus {
+    background-color: #289E00;
+    color: #ffffff;
+    font-size: 18px;
+    margin-left: -201px;
+  }
+
+  .btn-edit:active {
+    background-color: #289E00;
+    color: #ffffff;
+    font-size: 18px;
+    margin-left: -201px;
+  }
 </style>
