bcf17a19832ae5f0dd753e07f8b165968d39d699
lula <bodanli159951@163.com>
2019-10-14 15:20:29 +0800

    feat: 按需导入radio

--- src/views/ipfw/_tab-pane/DstIpPortSetting.vue
+++ src/views/ipfw/_tab-pane/DstIpPortSetting.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>目标IP端口</div>
+  <div class="dst-ip">
+    <div class="tcp">
+      <p>tcp</p>
+      <el-radio-group v-model="tcpRadio">
+        <el-radio label="ALL">允许所有端口</el-radio>
+        <el-radio label="NONE">阻断所有端口</el-radio>
+        <el-radio label="SELECTED">允许以下端口(逗号分隔)</el-radio>
+      </el-radio-group>
+    </div>
+    <div class="udp">
+      <p>udp</p>
+    </div>
+
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -7,12 +20,52 @@ export default {
   name: 'DstIpPortSetting',
   components: {},
   data() {
-    return {}
+    return {
+      tcpRadio: '',
+      ports: {
+        tcp: {
+          option: '',
+          selections: ''
+        },
+        udp: {
+          option: '',
+          selections: ''
+        }
+      },
+    }
   },
   computed: {},
-  created() {},
-  mounted() {},
-  methods: {},
+  watch: {
+    dialogVisible(val, oldVal) {
+      if (val) this.initCheckState()
+    }
+  },
+  created() { },
+  mounted() {
+    this.initCheckState()
+  },
+  methods: {
+    initCheckState() {
+      const {
+        tcp: {
+          option: tcpOption,
+          selections: tcpSelections
+        },
+        udp: {
+          option: udpOption,
+          selections: udpSelections
+        }
+      } = this.$store.state.sensor.tmpWhitelist.accessPorts
+      this.ports.tcp = {
+        option: tcpOption,
+        selections: tcpSelections
+      }
+      this.ports.udp = {
+        option: udpOption,
+        selections: udpSelections
+      }
+    }
+  },
 }
 </script>
 
